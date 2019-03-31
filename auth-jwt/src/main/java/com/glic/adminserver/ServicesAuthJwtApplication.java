3e4909d6f14699931e0e8e0c4c0dbe89dae08f00
Mauricio Fenoglio <mauricio.fenoglio>
2019-03-31 14:09:02 -0300

    Adding cors filter

--- auth-jwt/src/main/java/com/glic/adminserver/ServicesAuthJwtApplication.java
+++ auth-jwt/src/main/java/com/glic/adminserver/ServicesAuthJwtApplication.java
@@ -4,12 +4,28 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAsync
 public class ServicesAuthJwtApplication {
+
+   @Bean
+   CorsFilter corsFilter() {
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(true);
+      config.addAllowedOrigin("*");
+      config.addAllowedHeader("*");
+      config.addAllowedMethod("*");
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", config);
+      return new CorsFilter(source);
+   }
 
    public static void main(String[] args) {
       new SpringApplicationBuilder(ServicesAuthJwtApplication.class).web(WebApplicationType.SERVLET).run(args);
